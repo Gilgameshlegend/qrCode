acff599d7642e0e5e48a8c72d97f0c42db239802
1099335664@qq.com <109933564@qq.com>
2022-04-28 22:51:54 +0800

    update\= 7

--- src/main/java/com/tencent/wxcloudrun/WxCloudRunApplication.java
+++ src/main/java/com/tencent/wxcloudrun/WxCloudRunApplication.java
@@ -1,14 +1,22 @@
 package com.tencent.wxcloudrun;
 
+import com.tencent.wxcloudrun.controller.Schedule;
+import com.tencent.wxcloudrun.util.ContextUtil;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.tencent.wxcloudrun.dao"})
-public class WxCloudRunApplication {  
+public class WxCloudRunApplication {
+
+  private static ApplicationContext applicationContext;
+
 
   public static void main(String[] args) {
     SpringApplication.run(WxCloudRunApplication.class, args);
+    ContextUtil.getApplicationContext().getBean(Schedule.class).init();
   }
 }
