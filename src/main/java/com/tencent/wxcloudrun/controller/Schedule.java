cab8cf1fc1ef50cc80a191a2c4e2e9999d343193
1099335664@qq.com <109933564@qq.com>
2022-04-28 22:38:16 +0800

    update\= 6

--- src/main/java/com/tencent/wxcloudrun/controller/Schedule.java
+++ src/main/java/com/tencent/wxcloudrun/controller/Schedule.java
@@ -5,8 +5,11 @@ import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 
 @Component
@@ -17,9 +20,15 @@ public class Schedule {
 
     public static ScheduledExecutorService pool = new ScheduledThreadPoolExecutor(1, new BasicThreadFactory.Builder().namingPattern("refresh-pool-%d").daemon(true).build());
 
-//            public void init(){
-//                pool.scheduleWithFixedDelay(()-{
-//                        identityService
-//                });
-//            }
+    public void init() {
+        pool.scheduleWithFixedDelay(() -> {
+                identityService.deleteOutTimeCard(getCurrent());
+        },0,3, TimeUnit.SECONDS);
+    }
+
+
+    public static String getCurrent() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        return localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
 }
